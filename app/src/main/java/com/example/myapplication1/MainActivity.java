335a0a00c0b8c58cae5c4afdd31adbfb5db5983c
weijiecode <245619254@qq.com>
2020-09-12 23:46:29 +0800

    第一次提交

---
+++ app/src/main/java/com/example/myapplication1/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.myapplication1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button btn=(Button)findViewById(R.id.btnStartAnother);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,MainActivity2.class));
+            }
+        });
+    }
+}
